d5060871adf325cdca4126b27efcf404f0a42353
Leopold Stevens <79330042+leopomme@users.noreply.github.com>
2022-12-06 23:38:13 +0100

    Add files via upload

---
+++ kin.m
@@ -0,0 +1,10 @@
+function	x= kin(l,q);
+
+l1= l(1);	l2= l(2);
+q1= q(1);	q12= q1 + q(2);
+c1= cos(q1);	s1= sin(q1);
+c12= cos(q12);	s12= sin(q12);
+
+x=zeros(2,1);
+x(1)= l1*c1 + l2*c12;
+x(2)= l1*s1 + l2*s12;
